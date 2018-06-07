f32522b0232731b27dcbdff9e70e894022756b87
ga96dof <giulio.evangelisti@tum.de>
2018-06-07 19:36:57 +0200

    typo

--- applyQHe.m
+++ applyQHe.m
@@ -5,7 +5,7 @@ function Y = applyQHe(W,X)
 % to the vectors in the lower trianguler part of W to the matrix X such
 % that if W is result of a Householder QR-decomposition we have Y = Q'*X.
 %
-m = size(W,1);
+%m = size(W,1);
 n = size(W,2);
 Y = X;
 for i = 1:n
@@ -15,5 +15,5 @@ for i = 1:n
     %Y = [eye(i-1),zeros(i-1);zeros(n-i+1,i-1),F]*Y;
     Y = [eye(i-1),zeros(i-1,size(F,2));zeros(size(F,1),i-1),F]*Y;
     %}
-    Y(i:m,:) = Y(i:m,:) - 2*W(i:end,i)*W(i:end,i)'*Y(i:m,:);
+    Y(i:end,:) = Y(i:end,:) - 2*W(i:end,i)*W(i:end,i)'*Y(i:end,:);
 end
